cbfcb0d268659bab573e2554afa1eaffc523d60d
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2023-05-30 09:25:54 -0300

    Gradle configs

---
+++ buildLogic/settings.gradle.kts
@@ -0,0 +1,10 @@
+dependecyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
